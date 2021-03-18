ba4447fa2b810f78ab111d96b7121c0f9b5098cd
HLa-z <952898492@qq.com>
2021-03-18 16:19:51 +0800

    Update yq.cpp
    update 2

--- yq.cpp
+++ yq.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 #include<stdio.h>
 #include<string>
 #include<string.h>
@@ -10,43 +11,61 @@ struct Node{
 	string name, id;
 };
 
-char pathIn[105], pathOut[105], ri[10];
-
-int main(){
-	cout << "\n请输入命令  : ";
-	cin >> ri >> pathIn;
-	if(ri[0] != 'y' || ri[1] != 'q'){
-		cout << "无效命令!\n\n" << endl;
-		getchar();
+int main(int argc, char* argv[]){
+	if(argc < 3){
+		cout << "请输入文件名" << endl;
+		return 0;
+	}
+	//打开读入文件
+	ifstream infile;
+	infile.open(argv[1]);
+	if(!infile){
+		cout << "打开文件失败" << endl;
 		return 0;
 	}
 
-	//处理输入与输出文件的路径
-	int len = strlen(pathIn);
-	strcpy(pathOut, pathIn);
-	for(int i = len; i >= len-4; i--) pathOut[i] = pathOut[i-1];
-	len++; pathOut[len] = '\0';
-	pathOut[len-5] = 't'; pathOut[len-6] = 'u'; pathOut[len-7] = 'o';
-	freopen(pathIn, "r", stdin);
-	freopen(pathOut, "w", stdout);
+	//打开输出文件
+	ofstream outfile;
+	outfile.open(argv[2]);
+	if(!outfile){
+		cout << "打开文件失败" << endl;
+		return 0;
+	}
 
+	//读入文件内容
 	string s;
 	vector<string>vec, pro;
-	map<string, vector<Node> >mp;
+	while(infile >> s){
+		vec.push_back(s);
+	}
 
-	while(cin >> s) vec.push_back(s);
+	//实现分类
+	map<string, vector<Node> >mp;
 	int cnt = vec.size();
 	for(int i = 0; i < cnt; i += 3){
 		if(mp[vec[i]].size() == 0) pro.push_back(vec[i]);
 		mp[vec[i]].push_back({vec[i+1], vec[i+2]});
 	}
-	for(string i : pro){
-		cout << i << endl;
-		for(Node j : mp[i]){
-			cout << j.name << " " << j.id << endl;
-		}
-		cout << endl;
+
+	//输出到文件
+	if(argc == 4){ //如果参数有4个，即带有省份，则只输出该省份的城市
+		string t;
+		int len = strlen(argv[3]);
+		for(int i = 0; i < len; i++) t.push_back(argv[3][i]);
+		outfile << t << endl;
+		for(Node i : mp[t]) outfile << i.name << " " << i.id << endl;
 	}
-	system("pause");
+	else{ //否则全部输出
+		for(string i : pro){
+			outfile << i << endl;
+			for(Node j : mp[i]){
+				outfile << j.name << " " << j.id << endl;
+			}
+			outfile << endl;
+		}
+	}
+	infile.close();
+	outfile.close();
+	cout << "操作成功！" << endl;
 	return 0;
 }
